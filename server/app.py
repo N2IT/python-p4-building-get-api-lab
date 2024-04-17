e3fce8a136076b60f4f80ea5d555642a60cddf2b
Tony Eder <tonyeder11@gmail.com>
2024-04-17 12:04:47 -0700

    pausing for break

--- server/app.py
+++ server/app.py
@@ -20,19 +20,57 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    response = make_response(
+        bakeries,
+        200,
+        {"Content-Type" : "application/json"}
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        bakery_dict,
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+
+    # returns a list of baked goods as JSON
+    # sort list by price in descending order
+    
+    baked_goods = [baked_good.to_dict() for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+
+    response = make_response(
+        baked_goods,
+        200
+    )
+
+    return response
+    
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    prices = [baked_goods_by_price()]
+    highest_price = max(prices)
+
+    response = make_response(
+        highest_price,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
